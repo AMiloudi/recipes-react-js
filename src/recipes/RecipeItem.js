483c3e46520d96631390943216f83ce37b39297d
ANISSA <anissamiloudi@hotmail.nl>
2018-02-06 12:36:37 +0100

    a lot of things done.. tests recipes and recipeitems

---
+++ src/recipes/RecipeItem.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class RecipeItem extends PureComponent {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    vegan: PropTypes.bool,
+    vegetarian: PropTypes.bool,
+    pescatarian: PropTypes.bool,
+  }
+
+  render() {
+    const { title, summary, vegan, vegetarian, pescatarian } = this.props
+
+    return(
+      <article className="recipe">
+      <h1>{ title }</h1>
+      <div>
+      <p>{ summary }</p>
+      <ul>
+      { vegan && <li><span role="img" aria-label="vegan">🌾</span></li> }
+      { !vegan && vegetarian && <li><span role="img" aria-label="vegatarian">🥕</span></li> }
+      { pescatarian && <li><span role="img" aria-label="pescatarian">🐟</span></li> }
+      </ul>
+      </div>
+      </article>
+    )
+  }
+}
+
+export default RecipeItem
---
+++ src/recipes/RecipeItem.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import RecipeItem from './RecipeItem'
+
+const recipe = {
+  title: 'Spanish Omelette',
+  summary: 'A traditional dish from Spanish cuisine called tortilla española or tortilla de patatas. It is an omelette made with eggs and potatoes, sometimes also with onion and/or chives or garlic; fried in oil and often served cold as an appetizer.',
+  vegan: false,
+  vegetarian: true,
+  pescatarian: false,
+}
+
+describe('<RecipeItem />', () => {
+  const container = shallow(<RecipeItem { ...recipe } />)
+
+  it('is wrapped in a article tag with class name "recipe"', () => {
+    expect(container).toHaveTagName('article')
+    expect(container).toHaveClassName('recipe')
+  })
+
+  it('contains a the title', () => {
+    expect(container.find('h1')).toHaveText(recipe.title)
+  })
+
+  it('shows a 🥕  when it is vegetarian', () => {
+    expect(container.find('ul > li')).toHaveText('🥕')
+  })
+  it('shows a 🥕  when it is pescatarian', () => {
+    expect(container.find('ul > li')).toHaveText(🐟)
+  })
+  it('shows a 🥕  when it is vegan', () => {
+    expect(container.find('ul > li')).toHaveText(🌾)
+  })
+})
